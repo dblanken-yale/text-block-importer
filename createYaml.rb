b70d9a1727d06ba9bb17958403daf5422f5f746d
David Blankenship <david.blankenship@yale.edu>
2024-03-06 13:59:11 -0500

    Add some more comments

--- createYaml.rb
+++ createYaml.rb
@@ -12,6 +12,7 @@ end
 uri = URI.parse(ARGV[0])
 response = Net::HTTP.get_response(uri)
 
+# For permanent redirects, follow the redirect
 if response.is_a? Net::HTTPMovedPermanently then
   redirect_uri = URI.parse(response['location'])
   response = Net::HTTP.get_response(redirect_uri)
@@ -21,13 +22,16 @@ html = response.body
 doc = Nokogiri::HTML(html)
 html_output = doc.at(ARGV[1])
 
+# Attempt to find a h1 in the content to set as the title.  Make sure to encode
+# similarly to we will the html output.
 first_h1 = 
   begin
     doc.search("h1").map(&:text).first
   rescue => e
     ""
-  end
+  end.gsub("'", "&#39;")
 
+# So far only single quotes made us not work, so I'm encoding them.
 html_output = html_output.to_s.strip.gsub("\n", "").gsub("'", "&#39;")
 # Extract web page file name from URI
 web_page_file_name = File.basename(uri.path) || ""
